69dde4322cd8b16d71b1a529bc5994b2a36131fc
user <curtis.w.mceachen@gmail.com>
2022-12-06 16:22:09 -0500

    search functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Links
 import uuid
 # Create your views here.
@@ -11,10 +10,18 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        link = request.POST['link']
-        uid = str(uuid.uuid4())[:5]
-        new_link = Links(full_link=link,short_link=uid)
+        # create an object with the html input as the full link, and a random set of chars as the short link
+        # uuid4 returns a very long string of random chars, shorten it to 5 characters with [:5]
+        new_link = Links(full_link=request.POST['link'], short_link=str(uuid.uuid4())[:5])
         new_link.save()
         links = Links.objects.all()
-        print(links)
         return render(request, 'index.html', {'links': links})
+
+def search(request):
+    if request.method == 'POST':
+        try:
+            search_result = Links.objects.get(short_link=request.POST['shortened_link'])
+        except Links.DoesNotExist:
+            search_result = 'There are no records for that short link.'
+        links = Links.objects.all()
+        return render(request, 'index.html', {'links': links, 'search_result': search_result})
--- panitech/urls.py
+++ panitech/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import sys
-# sys.path.append('/Users/user/Library/Python/3.9/bin')
 
 from django.contrib import admin
 from django.urls import path, include
